136e0da0f6b20c2e375e1ee12fa52a7e988462d2
Atli Haraldsson <atlih4@gmail.com>
2018-02-14 13:39:32 +0000

    First commit

---
+++ app/src/main/java/is/hi/hopur16/nyttapp/rideActivity.java
@@ -0,0 +1,13 @@
+package is.hi.hopur16.nyttapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class rideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride);
+    }
+}
