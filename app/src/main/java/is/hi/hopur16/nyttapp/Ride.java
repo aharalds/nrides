5e776188e2cc2c76709336c3efc59074b3162345
Atli Haraldsson <atlih4@gmail.com>
2018-02-19 18:55:47 +0000

    DPFragment

---
+++ app/src/main/java/is/hi/hopur16/nyttapp/Ride.java
@@ -0,0 +1,9 @@
+package is.hi.hopur16.nyttapp;
+
+/**
+ * Created by atliharaldsson on 14/02/2018.
+ */
+
+public class Ride {
+
+}
--- app/src/main/java/is/hi/hopur16/nyttapp/rideActivity.java
+++ app/src/main/java/is/hi/hopur16/nyttapp/rideActivity.java
@@ -1,13 +1,52 @@
 package is.hi.hopur16.nyttapp;
 
+import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class rideActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class rideActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    EditText dateTxt;
+    EditText fromTxt;
+    EditText toTxt;
+    EditText costTxt;
+    EditText seatsTxt;
+    EditText timeTxt;
+    Button senda_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ride);
+
+        dateTxt = (EditText) findViewById(R.id.dateTxt);
+        dateTxt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    DialogFragment datePicker = new DatePickerFragment();
+                    datePicker.show(getSupportFragmentManager(), "date picker");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDate = DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(c.getTime());
+        dateTxt.setText(currentDate);
+
     }
 }
