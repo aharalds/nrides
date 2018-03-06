50d3317a118698db4d063f440edec355432ae7a5
Atli Haraldsson <atlih4@gmail.com>
2018-03-06 09:57:02 +0000

    +Jonas - POST

--- app/src/main/java/is/hi/hopur16/nyttapp/rideActivity.java
+++ app/src/main/java/is/hi/hopur16/nyttapp/rideActivity.java
@@ -7,13 +7,23 @@ import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.os.AsyncTask;
 
-import java.io.Serializable;
+import org.json.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DateFormat;
 import java.util.Calendar;
 
@@ -52,6 +62,7 @@ public class rideActivity extends AppCompatActivity implements DatePickerDialog.
                 Intent intent = new Intent(rideActivity.this, DisplayRideActivity.class);
                 Ride sendRide = createRide();
                 intent.putExtra("newRide", sendRide);
+
                 startActivity(intent);
 
             }
@@ -76,9 +87,86 @@ public class rideActivity extends AppCompatActivity implements DatePickerDialog.
         int costInt = Integer.parseInt(String.valueOf(costTxt.getText()));
         seatsTxt = (EditText) findViewById(R.id.seatsTxt);
         int seatsInt = Integer.parseInt(String.valueOf(seatsTxt.getText()));
-        dateTxt = (EditText) findViewById(R.id.seatsTxt);
+        dateTxt = (EditText) findViewById(R.id.dateTxt);
         timeTxt = (EditText) findViewById(R.id.timeTxt);
         ride = new Ride(fromTxt.getText().toString(),toTxt.getText().toString(), dateTxt.getText().toString(), timeTxt.getText().toString(), seatsInt, costInt);
+        JSONObject toPost = new JSONObject();
+        try {
+            toPost.put("rideFrom", fromTxt.getText().toString());
+            toPost.put("rideTo", toTxt.getText().toString());
+            toPost.put("date", dateTxt.getText().toString());
+            toPost.put("depTime", timeTxt.getText().toString());
+            toPost.put("seatsAvailable", seatsInt);
+            toPost.put("cost", costInt);
+            System.out.println(toPost.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (toPost.length() > 0) {
+            new SendJsonDataToServer().execute(String.valueOf(toPost));
+        }
         return ride;
     }
+
+    class SendJsonDataToServer extends AsyncTask<String,String,String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String JsonResponse = null;
+            String JsonDATA = params[0];
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL("http://nicerideserver.herokuapp.com");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestProperty("Accept", "application/json");
+                Writer writer = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8"));
+                writer.write(JsonDATA);
+                writer.close();
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String inputLine;
+                while ((inputLine = reader.readLine()) != null)
+                    buffer.append(inputLine + "\n");
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                JsonResponse = buffer.toString();
+                return JsonResponse;
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+        }
+
+    }
 }
+
+
+
