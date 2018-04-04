c678b56b4ca0241eb73b5d357792834b9d28876f
hornheidars <hornheidars@gmail.com>
2018-04-04 16:54:28 +0000

    Útlitslagfæringar
    Lagaði aðeins takkana og uppröðunina á xml skránum

--- app/src/main/java/is/hi/hopur16/nyttapp/homeActivity.java
+++ app/src/main/java/is/hi/hopur16/nyttapp/homeActivity.java
@@ -1,42 +1,26 @@
 package is.hi.hopur16.nyttapp;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.DateFormat;
-import java.util.Calendar;
+import android.widget.TextView;
 
 public class homeActivity extends AppCompatActivity  {
 
-
     Button searchRideBtn;
     Button addRideBtn;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setTextColor(Color.parseColor("#000000"));
 
         searchRideBtn = (Button) findViewById(R.id.searchRideBtn);
         addRideBtn = (Button) findViewById(R.id.addRideBtn);
