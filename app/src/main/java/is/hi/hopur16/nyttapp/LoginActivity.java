5aaec6fc9093fde3afbe369892f95a2233b8963c
hornheidars <hornheidars@gmail.com>
2018-04-09 21:15:12 +0000

    Login komið

--- app/src/main/java/is/hi/hopur16/nyttapp/LoginActivity.java
+++ app/src/main/java/is/hi/hopur16/nyttapp/LoginActivity.java
@@ -2,10 +2,12 @@ package is.hi.hopur16.nyttapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -13,6 +15,19 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by Hörn on 2.4.2018.
@@ -83,14 +98,12 @@ public class LoginActivity extends AppCompatActivity {
         loginInputLayoutUsername.setErrorEnabled(false);
         loginInputLayoutPassword.setErrorEnabled(false);
         if (checkUsername() && checkPassword()) {
-            Toast.makeText(getApplicationContext(), "Innskráning tókst!", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(LoginActivity.this, homeActivity.class);
-            startActivity(intent);
+            sendUser();
         }
     }
 
     /**
-     * Aðferð sem athugar hvort notandanafn sé til í gagnagrunni (og ekki tómt input frá notanda)
+     * Aðferð sem athugar hvort notandanafn sé gilt (ekki tómt)
      * @return
      */
     private boolean checkUsername() {
@@ -108,8 +121,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     /**
-     * Aðferð sem athugar hvort lykilorð passi við notandanafn í gagnagrunni (og sé ekki tómt input
-     * frá notanda)
+     * Aðferð sem athugar hvort lykilorð sé gilt (ekki tómt)
      * @return
      */
     private boolean checkPassword() {
@@ -123,11 +135,117 @@ public class LoginActivity extends AppCompatActivity {
         return true;
     }
 
+    // Aðferð sem setur fókus á ákveðið view
     private void requestFocus(View view) {
         if (view.requestFocus()) {
             getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
         }
     }
 
+    /**
+     * Aðferð sem sendir notandanafn og lykilorð á server
+     */
+    public void sendUser() {
+        String username = loginInputUsername.getText().toString();
+        String password = loginInputPassword.getText().toString();
+
+        JSONObject toPost = new JSONObject();
+        try {
+            toPost.put("username", username);
+            toPost.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (toPost.length() > 0) {
+            new LoginActivity.SendJsonDataToServer().execute(String.valueOf(toPost));
+        }
+    }
+
+    /**
+     * Aðferð sem tekur við svari frá server (true/false) og ýmist gefur villumeldingu
+     * eða sendir notanda áfram (ef innskráning tókst)
+     * @param response
+     */
+    public void checkUser(String response) {
+        try {
+            JSONObject json = new JSONObject(response);
+            Boolean success = json.getBoolean("success");
+
+            if (success == false) {
+                Toast.makeText(getApplicationContext(), "Innskráning mistókst! Notandanafn eða lykilorð ekki til.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Innskráning tókst!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, homeActivity.class);
+                startActivity(intent);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Klasi sem sendir login upplýsingar í gagnagrunn
+     */
+    class SendJsonDataToServer extends AsyncTask<String,String,String> {
+        String JsonResponse = null;
+        @Override
+        protected String doInBackground(String... params) {
+
+            String JsonDATA = params[0];
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL("https://nicerideserver.herokuapp.com/login");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestProperty("Accept", "application/json");
+                Writer writer = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8"));
+                writer.write(JsonDATA);
+                writer.close();
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String inputLine;
+                while ((inputLine = reader.readLine()) != null)
+                    buffer.append(inputLine + "\n");
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                JsonResponse = buffer.toString();
+
+                return JsonResponse;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.e("Response", " " + JsonResponse);
+            checkUser(JsonResponse);
+        }
+
+    }
 
 }
